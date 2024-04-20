fe715a07cc17a89cfaa75b21a1b7fc21388bc7d9
pooja <puja66802@gmail.com>
2024-04-20 13:28:42 +0530

    fetched selected chief. ce. ee

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,278 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Login Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class LoginPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LoginPageState extends State<LoginPage> {
+  String _selectedChief = '';
+  String _selectedSE = '';
+  String _password = '';
+  List<String> chiefs = [];
+  List<String> ses = [];
+  List<String> ees = [];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    fetchChiefs();
   }
+
+  Future<void> fetchChiefs() async {
+    print('----------------------------------');
+    final response =
+    await http.get(Uri.parse('http://117.250.2.226:6060/mobile/ce2'));
+    if (response.statusCode == 200) {
+      final responseData = json.decode(response.body);
+      if (responseData != null && responseData['data'] != null) {
+        setState(() {
+          chiefs = List<String>.from(responseData['data']['listOfCe']);
+        });
+      } else {
+        throw Exception('Failed to parse chiefs data');
+      }
+    } else {
+      throw Exception('Failed to load chiefs: ${response.statusCode}');
+    }
+  }
+
+  Future<void> fetchSEs(String chief) async {
+    final response =
+    await http.get(Uri.parse('http://117.250.2.226:6060/mobile/se/$chief'));
+    if (response.statusCode == 200) {
+      final responseData = json.decode(response.body);
+      if (responseData != null && responseData['data'] != null) {
+        List<String> tempList =
+        List<String>.from(responseData['data']['listOfSe']);
+        // Convert list to set to remove duplicates, then back to list
+        setState(() {
+          ses = tempList.toSet().toList(); // Remove duplicates
+        });
+      } else {
+        throw Exception('Failed to parse SEs data');
+      }
+    } else {
+      throw Exception('Failed to load SEs: ${response.statusCode}');
+    }
+  }
+
+  Future<void> fetchEEs(String se) async {
+    final response =
+    await http.get(Uri.parse('http://117.250.2.226:6060/mobile/ee/$se'));
+    if (response.statusCode == 200) {
+      final responseData = json.decode(response.body);
+      if (responseData != null && responseData['data'] != null) {
+        List<String> tempList =
+        List<String>.from(responseData['data']['listOfEe']);
+        setState(() {
+          ees = tempList.toSet().toList();
+        });
+      } else {
+        throw Exception('Failed to parse EEs data');
+      }
+    } else {
+      throw Exception('Failed to load EEs: ${response.statusCode}');
+    }
+  }
+
+  Future<void> login() async {
+    print('Logging in...');
+    final url = 'http://117.250.2.226:6060/mobile/login';
+    final response = await http.post(
+      Uri.parse(url),
+      body: {
+        'username': _selectedSE, // Use the selected SE as the username
+        'password': _password,
+      },
+    );
+    if (response.statusCode == 200) {
+      // Successful authentication
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => SuccessfulAuthPage(),
+        ),
+      );
+    } else if (response.statusCode == 401) {
+      // Unsuccessful authentication
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => UnsuccessfulAuthPage(),
+        ),
+      );
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Login Page'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Select Chief',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10.0),
+            DropdownButtonFormField(
+              value: _selectedChief.isNotEmpty ? _selectedChief : null,
+              items: chiefs.map((chief) {
+                return DropdownMenuItem(
+                  value: chief,
+                  child: Text(chief),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  _selectedChief = value!;
+                  fetchSEs(value);
+                });
+              },
+              decoration: InputDecoration(
+                labelText: 'Select Chief',
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            SizedBox(height: 20.0),
+            Text(
+              'Select SE',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10.0),
+            DropdownButtonFormField(
+              value: null,
+              items: ses.map((se) {
+                return DropdownMenuItem(
+                  value: se,
+                  child: Text(se),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  _selectedSE = value!;
+                  fetchEEs(value);
+                });
+              },
+
+              decoration: InputDecoration(
+                labelText: 'Select SE',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              'Select EE',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10.0),
+            DropdownButtonFormField(
+              value: null,
+              items: ees.map((ee) {
+                return DropdownMenuItem(
+                  value: ee,
+                  child: Text(ee),
+                );
+              }).toList(),
+              onChanged: (value) {
+                // Handle EE selection
+              },
+              decoration: InputDecoration(
+                labelText: 'Select EE',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              'Enter Password',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10.0),
+            TextFormField(
+              obscureText: true,
+              onChanged: (value) {
+                setState(() {
+                  _password = value;
+                });
+              },
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                login();
+                // Add your submit logic here
+              },
+              child: Text('Submit'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class SuccessfulAuthPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Successful Authentication'),
+      ),
+      body: Center(
+        child: Text(
+          'You have successfully authenticated!',
+          style: TextStyle(fontSize: 20.0),
+        ),
+      ),
+    );
+  }
+}
+
+class UnsuccessfulAuthPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Unsuccessful Authentication'),
+      ),
+      body: Center(
+        child: Text(
+          'Authentication failed. Please try again.',
+          style: TextStyle(fontSize: 20.0),
+        ),
+      ),
     );
   }
 }
